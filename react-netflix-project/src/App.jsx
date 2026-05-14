739ecfc546edd13cd16895f35ce4cf6f3634fe36
eityroy <eityroy027@gmail.com>
2026-05-14 23:42:45 +0600

    react netflix movie card project

--- react-netflix-project/src/App.jsx
+++ react-netflix-project/src/App.jsx
@@ -9,12 +9,29 @@ function App() {
 
    <div>
 
-  
+
+   <div><span>Name</span>:The hangover</div>  
+
+   <div><span>Rating </span>:7.7/10</div>
+
+   <div><span>Description:</span>A wild bachelor trip in Las Vegas turns into total chaos.</div>
 
    <div>
-    <img src="movie.jpg" alt="" />
+    <img src={ movie} alt="" />
    </div>
 
+ 
+   <div><span>Year:</span>2009</div>
+   <div><span>Duration:1h 40min</span>:2009</div>
+  
+   
+  <div>
+
+
+  <button>Play</button>
+  <button>Info</button>
+
+  </div>
 
    </div>
 
