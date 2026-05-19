cef04b8bbfaaf1e249207322696583e9c83fb3bb
eityroy <eityroy027@gmail.com>
2026-05-20 01:18:32 +0600

    react netflix movie card project

--- react-netflix-project/src/App.jsx
+++ react-netflix-project/src/App.jsx
@@ -1,77 +1,69 @@
-
-import series from "./api/Series.json"
+import series from "./api/Series.json";
 
 function App() {
- 
-  let age=16;
 
+  let age = 16;
 
-  //2nd way
-  // let Info="Info";
-  // if(age>=18) Info="Review";
+  const isAge = () => {
+    if (age >= 18)
+      return "Review";
 
-
-  const isAge =()=>{
-
-   if(age>=18)
-    return "Review";
-   
-
-   return "Not Available";
-
+    return "Not Available";
   };
-  
 
   return (
     <>
-     
+      <div className="flex flex-wrap justify-center gap-8 p-10">
 
-   <div className="h-screen flex justify-start items-center">
+        {series.map((item) => (
 
-   <div className="w-full h-[600px] flex flex-col justify-center items-center gap-3">
+          <div key={item.id} className="w-[400px] border p-4 rounded-xl shadow-lg">
 
+            <div className="w-full h-[300px] flex justify-center">
+              <img
+                className="w-[250px] h-[280px] rounded-md"
+                src={item.img_url}
+                alt={item.name}
+              />
+            </div>
 
-   <div className="w-full text-xl">
-    <span className="font-bold">Name:</span>The hangover
-    </div>  
+            <div className="w-full text-xl">
+              <span className="font-bold">Name:</span> {item.name}
+            </div>
 
-   <div className="w-full text-xl">
-    <span className="font-bold">Rating </span>:7.7/10
-    </div>
+            <div className="w-full text-xl">
+              <span className="font-bold">Rating:</span> {item.rating}
+            </div>
 
-   <div className="w-full text-xl">
-    <span className="font-bold">Description:</span>A wild bachelor trip in Las Vegas turns into total chaos.
-    </div>
-  
-   
-   <div className="w-full text-xl">
-    <span className="font-bold ">Year:</span>2009
-    </div>
+            <div className="w-full text-xl">
+              <span className="font-bold">Description:</span>
+              {item.description}
+            </div>
 
-   <div className="w-full text-xl">
-    <span className="font-bold ">Duration:1h 40min</span>
-    </div>
-  
+            <div className="w-full text-xl">
+              <span className="font-bold">Genre:</span>
+              {item.genre.join(", ")}
+            </div>
 
-  <div className="w-full h-[450px]">
-    <img className="w-[400px] h-[400px] rounded-md"  alt="" />
-   </div>
+            <div className="flex gap-4 mt-4">
 
-   
-  <div className="w-full flex gap-4">
+              <button className="px-2 py-2 border-2 bg-blue-100 text-sm text-blue-600 border-blue-600 rounded-4xl">
+                {age >= 18 ? "Watch Now" : "Not Available"}
+              </button>
 
-//1st way
-  <button className="px-2 py-2 border-2 bg-blue-100 text-sm text-blue-600 border-blue-600 rounded-4xl">{age>18? "Watch Now" : "Not Available"}</button>
-  <button className="px-6 py-2 border-2 bg-orange-100 text-sm text-orange-600 border-orange-600 rounded-3xl">{isAge()}</button>
+              <button className="px-6 py-2 border-2 bg-orange-100 text-sm text-orange-600 border-orange-600 rounded-3xl">
+                {isAge()}
+              </button>
 
-  </div>
+            </div>
 
-   </div>
-   </div>
+          </div>
 
+        ))}
 
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
